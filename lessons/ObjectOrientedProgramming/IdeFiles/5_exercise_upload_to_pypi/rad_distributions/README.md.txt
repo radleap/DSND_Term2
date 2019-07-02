e623ea3b71e07d7615ede2a3e74f8e9e90582baa
radleap <guy@radleap.com>
2019-07-01 20:37:33 -0400

    testing add large jpg file with lft

---
+++ lessons/ObjectOrientedProgramming/IdeFiles/5_exercise_upload_to_pypi/rad_distributions/README.md.txt
@@ -0,0 +1,7 @@
+# rad_distributions package
+
+Summary of package here ...
+
+# Files
+
+Explanation of files in the package
--- lessons/ObjectOrientedProgramming/IdeFiles/5_exercise_upload_to_pypi/setup.py
+++ lessons/ObjectOrientedProgramming/IdeFiles/5_exercise_upload_to_pypi/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='rad_distributions',
       version='0.1',
       description='Gaussian distributions',
-      packages=['distributions'],
+      packages=['rad_distributions'],
+      author = 'Radleap',
+      email = 'guy@radleap.com',
       zip_safe=False)
